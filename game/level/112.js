86316b6cb2706555f219d9c1b0ebbb50039cbc84
piztian <40465315+piztian@users.noreply.github.com>
2025-05-13 21:08:10 -0600

    Update 112.js

--- game/level/112.js
+++ game/level/112.js
@@ -1,6 +1,6 @@
 oS.Init(
 	{
-		PName: [oFlowerPot, oWallNut, oRepeater, oCherryBomb],
+		PName: [oWallNut, oFlowerPot, oRepeater, oCherryBomb],
 		ZName: [oCZombie, oCZombie2, oCZombie3, oCBucketheadZombie, oEunZombie, oCConeheadZombie, othugZombie, oZZ, oEmperor],
 		PicArr: ["images/interface/backgroundX4.jpg"],
 		LF: [0, 3, 0, 3, 0, 3, 0],
